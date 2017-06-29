d7e95b4d126d5cb01138d56649ebbf62d5b72505
liangzhimy <liangzhimy@gmail.com>
2017-06-30 07:07:47 +0800

    Initial Commit

---
+++ ZLTomatos/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ZLTomatos
+//
+//  Created by zhaoliang on 2017/6/30.
+//  Copyright © 2017年 zhaoliang. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ZLTomatos/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ZLTomatos
+//
+//  Created by zhaoliang on 2017/6/30.
+//  Copyright © 2017年 zhaoliang. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
